e4ab2ac4560dea0753795e0882d06e27a7dca6f4
Matt <matt@primestudiosllc.com>
2012-04-18 01:56:44 -0700

    removed encryption key

--- application/config/config.php
+++ application/config/config.php
@@ -233,7 +233,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'fWWQbggci7xT5GwanJR3UsFUPZfaGw0B';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
